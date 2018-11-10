1423664f6a795eda82ccefa9ba6b12b86560c5e9
Yoshiya-g <y.bskmelo7@gmail.com>
2018-11-11 01:57:13 +0900

    icon変更vuexinstall

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Vuex from 'vuex';
 import Hello from '@/components/pages/Hello';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(Router);
+Vue.use(Vuex);
 Vue.use(VueMaterial);
 
 export default new Router({
