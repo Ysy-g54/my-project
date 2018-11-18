ed77b3fab3eb1d5310806fb1186e30a8ea123252
Yoshiya-g <y.bskmelo7@gmail.com>
2018-11-19 01:18:13 +0900

    画像ファイル追加・処理修正

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,76 @@
 <template>
 	<div id="app">
 		<div class="page-container">
-		<md-app md-waterfall md-mode="fixed-last">
-			<md-app-toolbar v-show="$route.meta.currentHeader === 'GlobalHeader'" class="md-large md-dense md-primary">
-			<div class="md-toolbar-row">
-				<div class="md-toolbar-section-start">
-					<md-button class="md-icon-button" @click="showNavigation">
-						<md-icon class="menu"></md-icon>
-					</md-button>
-					<span class="md-title">memo-list</span>
+			<md-app md-waterfall md-mode="fixed-last">
+				<md-app-toolbar v-show="$route.meta.currentHeader === 'GlobalHeader'" class="md-large md-dense md-primary">
+					<div class="md-toolbar-row">
+						<div class="md-toolbar-section-start">
+							<md-button class="md-icon-button" @click="showNavigation">
+								<md-icon>
+									<md-avatar>
+										<img src="https://vuematerial.io/assets/examples/avatar.jpg" alt="Avatar">
+									</md-avatar>
+								</md-icon>
+							</md-button>
+						</div>
+
+						<div>
+							<md-field>
+								<md-icon class="search"></md-icon>
+								<label>検索条件</label>
+								<md-input v-model="initial"></md-input>
+							</md-field>
+						</div>
+
+						<md-menu class="md-toolbar-section-end" md-direction="bottom-start">
+							<md-button class="md-icon-button" md-menu-trigger>
+								<md-icon class="setting-detail"></md-icon>
+							</md-button>
+							<md-menu-content class="right">
+			    				<md-menu-item>My Item 1</md-menu-item>
+				    	    	<md-menu-item>My Item 2</md-menu-item>
+    							<md-menu-item>My Item 3</md-menu-item>
+							</md-menu-content>
+						</md-menu>
 					</div>
 
-				<md-menu class="md-toolbar-section-end" md-direction="bottom-start">
-					<md-button class="md-icon-button" md-menu-trigger>
-						<md-icon class="setting-detail"></md-icon>
-					</md-button>
-					<md-menu-content>
-		    			<md-menu-item>My Item 1</md-menu-item>
-			        	<md-menu-item>My Item 2</md-menu-item>
-    					<md-menu-item>My Item 3</md-menu-item>
-					</md-menu-content>
-				</md-menu>
-			</div>
+					<div class="md-toolbar-row">
+						<md-tabs md-sync-route>
+							<md-tab id="tab-home" md-label="Home"></md-tab>
+							<md-tab id="tab-pages" md-label="Pages"></md-tab>
+							<md-tab id="tab-posts" md-label="Posts"></md-tab>
+							<md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+						</md-tabs>
+					</div>
+				</md-app-toolbar>
 
-			<div class="md-toolbar-row">
-				<md-tabs md-sync-route>
-				<md-tab id="tab-home" md-label="Home"></md-tab>
-				<md-tab id="tab-pages" md-label="Pages"></md-tab>
-				<md-tab id="tab-posts" md-label="Posts"></md-tab>
-				<md-tab id="tab-favorites" md-label="Favorites"></md-tab>
-				</md-tabs>
-			</div>
-		</md-app-toolbar>
+				<md-app-drawer :md-active.sync="menuVisible">
+					<md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+					<md-list>
+						<md-list-item>
+							<md-icon>move_to_inbox</md-icon>
+							<span class="md-list-item-text">Inbox</span>
+						</md-list-item>
+						<md-list-item>
+							<md-icon>send</md-icon>
+							<span class="md-list-item-text">Sent Mail</span>
+						</md-list-item>
 
-		<md-app-drawer :md-active.sync="menuVisible">
-			<md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
-			<md-list>
-				<md-list-item>
-					<md-icon>move_to_inbox</md-icon>
-					<span class="md-list-item-text">Inbox</span>
-				</md-list-item>
-				<md-list-item>
-				<md-icon>send</md-icon>
-				<span class="md-list-item-text">Sent Mail</span>
-				</md-list-item>
+						<md-list-item>
+							<md-icon>delete</md-icon>
+							<span class="md-list-item-text">Trash</span>
+						</md-list-item>
 
-			<md-list-item>
-			<md-icon>delete</md-icon>
-			<span class="md-list-item-text">Trash</span>
-			</md-list-item>
-
-			<md-list-item>
-			<md-icon>error</md-icon>
-			<span class="md-list-item-text">Spam</span>
-			</md-list-item>
-		</md-list>
-			</md-app-drawer>
+						<md-list-item>
+							<md-icon>error</md-icon>
+							<span class="md-list-item-text">Spam</span>
+						</md-list-item>
+					</md-list>
+				</md-app-drawer>
 				<md-app-content>
 					<router-view></router-view>
 				</md-app-content>
-		</md-app>
+			</md-app>
 		</div>
 	</div>
 </template>
@@ -115,8 +126,8 @@ header span {
   box-sizing: border-box;
   padding-top: 16px;
 }
-.menu {
-  background: url("../static/img/icons/menu.png");
+.search {
+  background: url("../static/img/icons/search.png");
   background-size: cover;
 }
 .setting-detail {
