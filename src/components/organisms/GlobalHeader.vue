e5921c2e3eda7e4d8161e1a0c9333fce6af849b9
Yoshiya-g <y.bskmelo7@gmail.com>
2018-12-24 23:26:56 +0900

    ファイル追加

---
+++ src/components/organisms/GlobalHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="md-toolbar-row">
+    <md-tabs md-sync-route>
+      <md-tab id="tab-home" md-label="Home"></md-tab>
+      <md-tab id="tab-pages" md-label="Pages"></md-tab>
+      <md-tab id="tab-posts" md-label="Posts"></md-tab>
+      <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+    </md-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GlobalHeader",
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
