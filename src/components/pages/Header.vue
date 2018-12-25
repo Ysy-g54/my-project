08ccdcc4bccec52f0dd8800a4b29aec888c12bea
Yoshiya-g <y.bskmelo7@gmail.com>
2018-12-26 01:13:52 +0900

    addfile

---
+++ src/components/pages/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div name="header">
+    <global-header></global-header>
+  </div>
+</template>
+
+<script>
+import GlobalHeader from "@/components/organisms/GlobalHeader";
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {
+    GlobalHeader
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
