80fdfac5a2b8890d01a40c24f02cf50d228090da
yoshiya.gibo <yoshiya.gibo@persol.co.jp>
2019-01-15 11:24:39 +0900

    デザイン修正

--- src/App.vue
+++ src/App.vue
@@ -15,35 +15,4 @@ export default {
 </script>
 
 <style scoped>
-body {
-  margin: 0;
-}
-
-.md-app {
-  max-height: 400px;
-  border: 1px solid rgba(#000, 0.12);
-}
-
-.md-drawer {
-  width: 230px;
-  max-width: calc(100vw - 125px);
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495e;
-  color: #ffffff;
-}
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: 0.02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
