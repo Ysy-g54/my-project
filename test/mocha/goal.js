91d520dfe6f3c3874ab993884fd460c6e38c0ecb
Yoshiya-g <y.bskmelo7@gmail.com>
2019-01-24 01:44:54 +0900

    utdemo

---
+++ test/mocha/goal.js
@@ -0,0 +1,18 @@
+// import Vue from 'vue';
+
+/* eslint-disable no-new */
+// new Vue({
+// 	el: '#app',
+// 	router,
+// 	template: '<App/>',
+// 	components: { App }
+// });
+
+var assert = require('assert');
+describe('Array',() => {
+	describe('#indexOf()',() => {
+		it('should return -1 when the value is not present',() => {
+			assert.equal([1,2,3].indexOf(4),-1);
+		});
+	});
+});
