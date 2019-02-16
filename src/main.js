f2cb228984608284da84ecc388600c14584019b6
Yoshiya-g <y.bskmelo7@gmail.com>
2019-02-17 02:30:02 +0900

    ref

--- src/main.js
+++ src/main.js
@@ -4,17 +4,18 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import firebase from 'firebase';
+import process from '../.env';
 
 Vue.config.productionTip = false;
 
 // Initialize Firebase
 const config = {
-	apiKey: "AIzaSyAiD2rUjrmntKbEJjBQafnQVNTh_MLXqjI",
-	authDomain: "memo-d707b.firebaseapp.com",
-	databaseURL: "https://memo-d707b.firebaseio.com",
-	projectId: "memo-d707b",
-	storageBucket: "memo-d707b.appspot.com",
-	messagingSenderId: "1056391428197"
+	apiKey: process.apiKey,
+	authDomain: process.authDomain,
+	databaseURL: process.databaseURL,
+	projectId: process.projectId,
+	storageBucket: process.storageBucket,
+	messagingSenderId: process.messagingSenderId
 };
 firebase.initializeApp(config);
 
