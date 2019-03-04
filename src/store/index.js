473fefd4379e5a74a43c47283126bee71b8b6791
Yoshiya-g <y.bskmelo7@gmail.com>
2019-03-05 00:38:59 +0900

    refs

--- src/store/index.js
+++ src/store/index.js
@@ -2,42 +2,52 @@ import firebase from "firebase";
 import Vuex from "vuex";
 
 const Login = {
-  namespaced: true,
-  state: {
-    loginUser: {
-      mailAddress: null
-    }
-  },
-  mutations: {
-    setLoginUser(state) {
-      let loginUser = firebase.auth().currentUser;
-      state.loginUser.mailAddress = loginUser.email;
-    }
-  },
-  actions: {
-    updateLoginUser(context, params) {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(params.mailAddress, params.password)
-        .then(() => {
-          context.commit("setLoginUser");
-          return firebase.auth().currentUser;
-        });
-    },
-    findLoginUser(context) {
-      context.commit("setLoginUser");
-      return firebase.auth().currentUser;
-    }
-  },
-  getters: {
-    getLoginUser(state) {
-      return state.loginUser;
-    }
-  }
+	namespaced: true,
+	state: {
+		loginUser: {
+			name: '',
+			mailAddress: '',
+			photoUrl: '',
+			uid: '',
+			emailVerified: ''
+		}
+	},
+	mutations: {
+		setLoginUser(state) {
+			let loginUser = firebase.auth().currentUser;
+			if (loginUser != null) {
+				state.loginUser.name = loginUser.displayName;
+				state.loginUser.mailAddress = loginUser.email;
+				state.loginUser.photoUrl = loginUser.photoUrl;
+				state.loginUser.uid = loginUser.uid;
+				state.loginUser.emailVerified = loginUser.emailVerified;
+			}
+		}
+	},
+	actions: {
+		updateLoginUser(context, params) {
+			firebase
+				.auth()
+				.signInWithEmailAndPassword(params.mailAddress, params.password)
+				.then(() => {
+					context.commit("setLoginUser");
+					return firebase.auth().currentUser;
+				});
+		},
+		findLoginUser(context) {
+			context.commit("setLoginUser");
+			return firebase.auth().currentUser;
+		}
+	},
+	getters: {
+		getLoginUser(state) {
+			return state.loginUser;
+		}
+	}
 };
 
 export default new Vuex.Store({
-  modules: {
-    Login
-  }
+	modules: {
+		Login
+	}
 });
