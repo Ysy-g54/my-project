3c80fbcdfa0f99e9b0de96e516ccc64fb69c7aa9
Yoshiya-g <y.bskmelo7@gmail.com>
2019-07-28 01:05:52 +0900

    refs画像アップロード(firebasestrageにはまだ登録する処理未実装)

--- src/components/organisms/MemoModification.vue
+++ src/components/organisms/MemoModification.vue
@@ -4,8 +4,20 @@
       <label>メモ内容</label>
       <md-textarea v-autofocus v-model="memo"></md-textarea>
     </md-field>
+    <div v-show="isShowFile">
+      <img v-for="file in files" :key="file.id" :src="file.thumb" width="40" height="auto" />
+    </div>
     <md-button class="md-icon-button">
-      <md-icon>insert_photo</md-icon>
+      <fileUpload
+        ref="upload"
+        v-model="files"
+        post-action="/post.method"
+        put-action="/put.method"
+        @input-file="inputFile"
+        @input-filter="inputFilter"
+      >
+        <md-icon>insert_photo</md-icon>
+      </fileUpload>
     </md-button>
     <md-button
       class="md-icon-button"
@@ -32,6 +44,8 @@
 
 <script>
 import firebase from "firebase";
+import fileUpload from "vue-upload-component";
+// import ImageCompressor from "@xkeshi/image-compressor";
 import { categories } from "../../constants";
 import "firebase/firestore";
 export default {
@@ -45,7 +59,7 @@ export default {
       insertDateTime: null,
       categories: categories,
       favoriteFlg: false,
-      file: null,
+      files: [],
       doneFlg: false
     };
   },
@@ -87,6 +101,84 @@ export default {
         .catch(error => {
           console.error("Error adding document: ", error);
         });
+    },
+    /**
+     * Has changed
+     * @param  Object|undefined   newFile   Read only
+     * @param  Object|undefined   oldFile   Read only
+     * @return undefined
+     */
+    inputFile: function(newFile, oldFile) {
+      if (newFile && oldFile && !newFile.active && oldFile.active) {
+        // Get response data
+        console.log("response", newFile.response);
+        if (newFile.xhr) {
+          //  Get the response status code
+          console.log("status", newFile.xhr.status);
+        }
+      }
+      newFile.thumb = "";
+      if (newFile.blob && newFile.type.substr(0, 6) === "image/") {
+        newFile.thumb = newFile.blob;
+      }
+    },
+    /**
+     * Pretreatment
+     * @param  Object|undefined   newFile   Read and write
+     * @param  Object|undefined   oldFile   Read only
+     * @param  Function           prevent   Prevent changing
+     * @return undefined
+     */
+    inputFilter: function(newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // Before adding a file
+        // Filter system files or hide files
+        if (/(\/|^)(Thumbs\.db|desktop\.ini|\..+)$/.test(newFile.name)) {
+          return prevent();
+        }
+        // Filter php html js file
+        if (/\.(php5?|html?|jsx?)$/i.test(newFile.name)) {
+          return prevent();
+        }
+        // // Automatic compression
+        // if (
+        //   newFile.file &&
+        //   newFile.type.substr(0, 6) === "image/" &&
+        //   this.autoCompress > 0 &&
+        //   this.autoCompress < newFile.size
+        // ) {
+        //   newFile.error = "compressing";
+        //   const imageCompressor = new ImageCompressor(null, {
+        //     convertSize: Infinity,
+        //     maxWidth: 512,
+        //     maxHeight: 512
+        //   });
+        //   imageCompressor
+        //     .compress(newFile.file)
+        //     .then(file => {
+        //       this.$refs.upload.update(newFile, {
+        //         error: "",
+        //         file,
+        //         size: file.size,
+        //         type: file.type
+        //       });
+        //     })
+        //     .catch(err => {
+        //       this.$refs.upload.update(newFile, {
+        //         error: err.message || "compress"
+        //       });
+        //     });
+        // }
+      }
+
+      if (newFile && (!oldFile || newFile.file !== oldFile.file)) {
+        // Create a blob field
+        newFile.blob = "";
+        let URL = window.URL || window.webkitURL;
+        if (URL && URL.createObjectURL) {
+          newFile.blob = URL.createObjectURL(newFile.file);
+        }
+      }
     }
   },
   watch: {
@@ -94,7 +186,11 @@ export default {
       this.saveMemo();
     }
   },
-  computed: {},
+  computed: {
+    isShowFile() {
+      return this.files.length !== 0;
+    }
+  },
   props: {
     isSavable: { type: Boolean, default: false }
   },
@@ -120,7 +216,7 @@ export default {
     }
   },
   components: {
-    inject: ["$validator"]
+    fileUpload
   }
 };
 </script>
