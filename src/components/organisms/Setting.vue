0c051b7e50b9c16049d3055f87b33ef8630ff711
Yoshiya-g <y.bskmelo7@gmail.com>
2019-08-22 00:21:43 +0900

    refs設定更新機能実装

--- src/components/organisms/Setting.vue
+++ src/components/organisms/Setting.vue
@@ -1,23 +1,50 @@
 <template>
   <div>
     <md-subheader>メモの表示形式</md-subheader>
-    <md-radio v-model="radio" :value="'0'" class="md-primary">カード</md-radio>
-    <md-radio v-model="radio" :value="'1'" class="md-primary">リスト</md-radio>
+    <md-radio v-model="memoDisplayForm" :value="'0'" class="md-primary">カード</md-radio>
+    <md-radio v-model="memoDisplayForm" :value="'1'" class="md-primary">リスト</md-radio>
+    <div class="md-layout md-alignment-top-right">
+      <md-button class="md-dense md-raised md-primary" @click="saveSetting">保存</md-button>
+    </div>
+    <Snackbar ref="snackbar" :message="'更新しました'"></Snackbar>
   </div>
 </template>
 
 <script>
+import Snackbar from "@/components/atoms/Snackbar";
+import firebase from "firebase";
+import "firebase/firestore";
 export default {
   data() {
     return {
-      radio: this.$store.getters["getLoginUser"].memoDisplayForm
+      memoDisplayForm: this.$store.getters["getLoginUser"].memoDisplayForm
     };
   },
-  methods: {},
+  methods: {
+    saveSetting() {
+      firebase
+        .firestore()
+        .collection("userSetting")
+        .doc(this.$store.getters["getLoginUser"].userSettingId)
+        .update({
+          memoDisplayForm: this.memoDisplayForm
+        })
+        .then(() => {
+          this.$store.dispatch("findLoginUser").then(() => {
+            this.$refs.snackbar.openSnackbar();
+          });
+        })
+        .catch(error => {
+          console.error("Error adding document: ", error);
+        });
+    }
+  },
   computed: {},
   mounted() {},
   created() {},
-  components: {}
+  components: {
+    Snackbar
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,76 +2,79 @@ import firebase from "firebase";
 import Vuex from "vuex";
 
 const store = new Vuex.Store({
-  namespaced: true,
-  state: {
-    loginUser: {
-      displayName: "",
-      mailAddress: "",
-      photoUrl: "",
-      uid: "",
-      emailVerified: "",
-      memoDisplayForm: ""
-    },
-    loading: false
-  },
-  mutations: {
-    async setLoginUser(state) {
-      await firebase.auth().onAuthStateChanged(loginUser => {
-        state.loginUser.displayName = loginUser.displayName || "";
-        state.loginUser.mailAddress = loginUser.email || "";
-        state.loginUser.photoUrl = loginUser.photoURL || "";
-        state.loginUser.uid = loginUser.uid || "";
-        state.loginUser.emailVerified = loginUser.emailVerified || "";
-        firebase
-          .firestore()
-          .collection("userSetting")
-          .where("uid", "==", state.loginUser.uid)
-          .get()
-          .then(querySnapshot => {
-            querySnapshot.forEach(document => {
-              state.loginUser.memoDisplayForm = document.data().memoDisplayForm;
-            });
-          });
-      });
-    },
-    logout(state) {
-      state.loginUser.displayName = "";
-      state.loginUser.mailAddress = "";
-      state.loginUser.photoUrl = "";
-      state.loginUser.uid = "";
-      state.loginUser.emailVerified = "";
-      state.loginUser.memoDisplayForm = "";
-    },
-    setIsLoading(state, flg) {
-      state.loading = flg;
-    }
-  },
-  actions: {
-    updateLoginUser(context, params) {
-      return firebase
-        .auth()
-        .signInWithEmailAndPassword(params.mailAddress, params.password)
-        .then(() => {
-          context.commit("setLoginUser");
-        });
-    },
-    async findLoginUser(context) {
-      await context.commit("setLoginUser");
-    },
-    logout(context) {
-      return firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          context.commit("logout");
-        });
-    }
-  },
-  getters: {
-    getLoginUser: state => {
-      return state.loginUser;
-    }
-  }
+	namespaced: true,
+	state: {
+		loginUser: {
+			displayName: "",
+			mailAddress: "",
+			photoUrl: "",
+			uid: "",
+			emailVerified: "",
+			memoDisplayForm: "",
+			userSettingId: ""
+		},
+		loading: false
+	},
+	mutations: {
+		async setLoginUser(state) {
+			await firebase.auth().onAuthStateChanged(loginUser => {
+				state.loginUser.displayName = loginUser.displayName || "";
+				state.loginUser.mailAddress = loginUser.email || "";
+				state.loginUser.photoUrl = loginUser.photoURL || "";
+				state.loginUser.uid = loginUser.uid || "";
+				state.loginUser.emailVerified = loginUser.emailVerified || "";
+				firebase
+					.firestore()
+					.collection("userSetting")
+					.where("uid", "==", state.loginUser.uid)
+					.get()
+					.then(querySnapshot => {
+						querySnapshot.forEach(document => {
+							state.loginUser.memoDisplayForm = document.data().memoDisplayForm;
+							state.loginUser.userSettingId = document.id;
+						});
+					});
+			});
+		},
+		logout(state) {
+			state.loginUser.displayName = "";
+			state.loginUser.mailAddress = "";
+			state.loginUser.photoUrl = "";
+			state.loginUser.uid = "";
+			state.loginUser.emailVerified = "";
+			state.loginUser.memoDisplayForm = "";
+			state.loginUser.userSettingId = "";
+		},
+		setIsLoading(state, flg) {
+			state.loading = flg;
+		}
+	},
+	actions: {
+		updateLoginUser(context, params) {
+			return firebase
+				.auth()
+				.signInWithEmailAndPassword(params.mailAddress, params.password)
+				.then(() => {
+					context.commit("setLoginUser");
+				});
+		},
+		async findLoginUser(context) {
+			await context.commit("setLoginUser");
+		},
+		logout(context) {
+			return firebase
+				.auth()
+				.signOut()
+				.then(() => {
+					context.commit("logout");
+				});
+		}
+	},
+	getters: {
+		getLoginUser: state => {
+			return state.loginUser;
+		}
+	}
 });
 
 export default store;
