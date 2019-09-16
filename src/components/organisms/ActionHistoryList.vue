e121e832ca34383ba2411e075e62baee2401d656
Yoshiya-g <y.bskmelo7@gmail.com>
2019-09-16 18:48:51 +0900

    refsファイル作成

---
+++ src/components/organisms/ActionHistoryList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-for="actionHistory in actionHistorys" :key="actionHistory.actionHistoryId">
+      <md-list class="md-double-line">
+        <md-list-item>
+          <div class="md-list-item-text">
+            <p>{{ "テスト" }}</p>
+          </div>
+        </md-list-item>
+        <md-divider></md-divider>
+      </md-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+import firebase from "firebase";
+import "firebase/firestore";
+export default {
+  data() {
+    return {
+      actionHistorys: [],
+      database: firebase.firestore()
+    };
+  },
+  methods: {
+    async searchActionHistory() {
+      await this.database
+        .collection("actionHistory")
+        .where("userId", "==", this.$store.getters["getLoginUser"].uid)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(document => {
+            let data = document.data();
+            _.set(data, "actionHistoryId", document.id);
+            this.actionHistorys.push(data);
+          });
+        });
+    }
+  },
+  watch: {},
+  computed: {},
+  created() {
+    this.searchActionHistory();
+  },
+  components: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
