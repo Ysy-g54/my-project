2a600fd9a4602dfecff0d6f24c0264134295565b
Yoshiya-g <y.bskmelo7@gmail.com>
2020-02-02 17:19:01 +0900

    ファイル名修正

---
+++ src/components/molecules/DialogConfirm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <md-dialog-confirm
+      :md-active.sync="active"
+      :md-title="title"
+      md-confirm-text="OK"
+      md-cancel-text="キャンセル"
+      @md-confirm="onConfirmClick"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    active: false
+  }),
+  props: {
+    title: { type: String, default: "" }
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    onConfirmClick() {
+      this.$emit("confirm-dialog");
+    },
+    openDialog() {
+      this.active = true;
+    }
+  },
+  mounted() {},
+  created() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
